dcf866377f9598eb29b985ab8e4c43536b906fd2
Sunil Rao <raosg@rose-hulman.edu>
2016-04-27 13:55:51 -0400

    Updated all verilog files from changes made for milestone 2 in virtuoso

--- Verilog/fmac_behav.v
+++ Verilog/fmac_behav.v
@@ -1,6 +1,6 @@
-module fmac(input CLK, input RESET, input [7:0] x, input [7:0] y, output reg [15:0] a);
-
-parameter THRESHOLD = 16'd65025;
+// Created by ihdl
+module fmac(input CLK, input RESET, input [7:0] x, input [7:0] y, 
+			input [15:0] threshold, output reg [15:0] a);
 
 reg [15:0] accumulator;
 
@@ -17,7 +17,7 @@ always @(posedge CLK) begin
     if (RESET == 0) begin
         a <= 16'd0;
         accumulator <= 16'd0;
-    end else if (a > THRESHOLD) begin
+    end else if (a > threshold) begin
         a <= 16'd0;
         accumulator <= 16'd0;
     end else begin
--- Verilog/shifter_func.v
+++ Verilog/shifter_func.v
@@ -1,13 +1,14 @@
-module Shifter(input [2:0] action, input [7:0] multplicand, output [8:0] partialProduct);
-//en0 = single = action[0] en1 = double = action[1] en2 = neg = action[2]
-	BoothDecoder1bit decode0(.ya(multplicand[0]),.yb(1'b0),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[0]));
-	BoothDecoder1bit decode1(.ya(multplicand[1]),.yb(multplicand[0]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[1]));
-	BoothDecoder1bit decode2(.ya(multplicand[2]),.yb(multplicand[1]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[2]));
-	BoothDecoder1bit decode3(.ya(multplicand[3]),.yb(multplicand[2]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[3]));
-	BoothDecoder1bit decode4(.ya(multplicand[4]),.yb(multplicand[3]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[4]));
-	BoothDecoder1bit decode5(.ya(multplicand[5]),.yb(multplicand[4]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[5]));
-	BoothDecoder1bit decode6(.ya(multplicand[6]),.yb(multplicand[5]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[6]));
-	BoothDecoder1bit decode7(.ya(multplicand[7]),.yb(multplicand[6]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[7]));
-	BoothDecoder1bit decode8(.ya(multplicand[8]),.yb(multplicand[7]),.en0(action[0]),.en1(action[1]),.en2(action[2]),.ppj(partialProduct[8]));
+//Verilog HDL for "551FMAC", "shifter" "behavioral"
 
+//selects the correct partial product from the encoder output
+module Shifter(input [2:0] action, input [7:0] multiplicand, output reg [9:0] partialProduct);
+always@(action or multiplicand)
+	case(action)
+		3'b000: partialProduct<=10'd0;
+		3'b010: partialProduct<={2'b00,multiplicand}<<1;
+		3'b011: partialProduct<=~({1'b0,multiplicand,1'b0})+1'b1;
+		3'b100: partialProduct<={2'b00,multiplicand};
+		3'b101: partialProduct<=(~{2'b00,multiplicand})+1'b1;
+		default: partialProduct<=multiplicand;
+	endcase
 endmodule
